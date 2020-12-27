fb935a4c2b152bf84032b7b1f5769e856ad0f482
Showmick Kar <showmick12@gmail.com>
2020-12-27 20:00:55 +0600

    Update main.py
    Added Dijkstra's Algorithm

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def reconstructPath(came_from, current, draw):
         draw()
 
 
-def algorithm(draw, grid, start, end):
+def aStar(draw, grid, start, end):
     count = 0
     priority_queue = PriorityQueue()
     priority_queue.put((0, count, start))
@@ -137,6 +137,44 @@ def algorithm(draw, grid, start, end):
         if current != start:
             current.makeVisited()
     return False
+
+
+def dijkstra(draw, grid, start, end):
+    visited = {node: False for row in grid for node in row}
+    distance = {node: math.inf for row in grid for node in row}
+    distance[start] = 0
+    came_from = {}
+    priority_queue = PriorityQueue()
+    priority_queue.put((0, start))
+    while not priority_queue.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        current = priority_queue.get()[1]
+
+        if visited[current]:
+            continue
+        visited[current] = True
+        if current == end:
+            reconstructPath(came_from, end, draw)
+            return True
+        if current != start:
+            current.makeVisited()
+        for neighbor in current.neighbors:
+            weight = 1
+            if distance[current] + weight < distance[neighbor]:
+                came_from[neighbor] = current
+                distance[neighbor] = distance[current] + weight
+                priority_queue.put((distance[neighbor], neighbor))
+            if neighbor != end and neighbor != start and not visited[neighbor]:
+                neighbor.makeVisiting()
+        draw()
+    return False
+
+
+def algorithm(draw, grid, start, end):
+    # aStar(draw, grid, start, end)
+    dijkstra(draw, grid, start, end)
 
 
 def huresticFunction(intermediate_node, end_node):
